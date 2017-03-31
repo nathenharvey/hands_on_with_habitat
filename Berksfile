88abd100e26a7a2d2c100cf48456162e69454773
Nathen Harvey <nharvey@chef.io>
2017-03-31 10:39:18 -0400

    Depend on habitat_workstation cookbook
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.chef.io'
 
 metadata
+
+cookbook 'habitat_workstation', git: 'git@github.com:chef-cft/habitat_workstation.git', rel: 'cookbooks/habitat_workstation'
--- metadata.rb
+++ metadata.rb
@@ -5,16 +5,7 @@ license 'Apache-2.0'
 description 'Configures a node for Hands on with Habitat Workshop'
 long_description 'Configures a node for Hands on with Habitat Workshop'
 version '0.1.0'
-depend 'habitat'
+issues_url 'https://github.com/nathenharvey/hands_on_with_habitat/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/nathenharvey/hands_on_with_habitat' if respond_to?(:source_url)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/hands_on_with_habitat/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/hands_on_with_habitat' if respond_to?(:source_url)
+depends 'habitat_workstation'
